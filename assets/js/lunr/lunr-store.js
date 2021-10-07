b4b69dbe956b0e5a5ab8c016bb7c08fb8974b624
zamzterz <zamzterz@users.noreply.github.com>
2021-10-07 15:01:00 +0000

    deploy: 1565380e030eb73e1874b2c7c76d651c6098cace

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Sommar i Norrlands inland",
+        "excerpt":"Längs en väg som aldrig tar slut, över ett lapptäcke av mörk och ljus asfaltslagning. Så dånar den nya gamla sommarbilen förbi idyllen med ängsblom och svartvitfläckiga kor, i myggsvärmar och kall juni. Tall efter tall, efter gran efter tall. Oj, en björk. Sen tall, gran, tall, björk, asp, och...","categories": ["text"],
+        "tags": [],
+        "url": "/text/2019/06/25/sommar-i-norrlands-inland.html",
+        "teaser": null
+      }]
