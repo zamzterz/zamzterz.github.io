a33ec58f2826b55c103561fad7ce37cf2b168bd1
zamzterz <zamzterz@users.noreply.github.com>
2021-10-11 20:25:20 +0000

    deploy: ae3862f5d8a6321d286d9a6348e5870d2b25a5d3

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/text/2019/06/25/sommar-i-norrlands-inland.html",
         "teaser": null
+      },{
+        "title": "När natten tynger",
+        "excerpt":"När natten tynger lättar mörkret ankar. En storm genom allt som är jag sliter i alla trasiga trådar som håller ihop mig. Tyngd av ånger och sten ånger och sten ånger och sten ångesten. Jag dras till djup djupare än Marianergraven. Men jag vänder om till värmen från din rygg....","categories": ["text"],
+        "tags": [],
+        "url": "/text/2021/09/15/n%C3%A4r-natten-tynger.html",
+        "teaser": null
       }]
